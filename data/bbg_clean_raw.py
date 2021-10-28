40e56d4c3ef16d9a2934a6182e2920475b924a75
fisertiz <fisertiz@spekulatius.wiwi.hu-berlin.de>
2021-10-28 21:41:38 +0200

    update

---
+++ data/bbg_clean_raw.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 10 05:53:03 2021
+
+@author: ms
+"""
+import os
+import tqdm
+import platform
+import numpy as np
+import pandas as pd
+from openpyxl import load_workbook
+pd.set_option('display.max_columns', None)
+
+if platform.node() in ['mstp3', 'msbq']:
+    os.chdir('/home/ms/github/fxpred/data')
+
+fp_raw = 'bbg_raw'
+fp_clean = 'bbg'
+
+if not os.path.isdir(fp_clean):
+    os.makedirs(fp_clean)
+
+
+for fp in tqdm.tqdm(os.listdir(fp_raw)):
+    x = pd.read_excel(os.path.join(fp_raw, fp))
+    x2 = pd.read_excel(os.path.join(fp_raw + '2', fp))
+
+    # delete all columns without input
+    x = x.loc[:, ~x.isna().all(axis=0)]
+    x2 = x2.loc[:, ~x2.isna().all(axis=0)]
+    fx_name = x.columns[0].replace(' Curncy', '')
+    fx_name2 = x2.columns[0].replace(' Curncy', '')
+
+    # rename columns
+    x.columns = x.iloc[1, :]
+    x2.columns = x2.iloc[1, :]
+
+    # delete first 2 lines
+    x = x.iloc[2:, :]
+    x2 = x2.iloc[2:, :]
+
+    # checking that all dates are same
+    def clean(x):
+        dates = np.where(x.columns == 'Dates')[0]
+        if np.all([(x.iloc[:, dates[0]] == x.iloc[:, i]).all() for i in dates]):
+            # set index to Dates
+            x.index = x.iloc[:, dates[0]]
+            # deleting all Dates columns
+            res = x.loc[:, x.columns != 'Dates']
+            res.columns.name = None
+        else:
+            res = None
+            for i, c in enumerate(dates):
+                if i < len(dates) - 1:
+                    c_end = dates[i + 1]
+                else:
+                    c_end = x.shape[1]
+                tmp = x.iloc[2:, c:c_end].dropna(how='all')
+                tmp.index = pd.to_datetime(tmp.Dates)
+                tmp = tmp.drop('Dates', axis=1)
+                # tmp = pd.DataFrame(tmp[:, 1:], index=tmp.Dates)
+                # tmp = pd.DataFrame(x.iloc[2:, c + 1:c_end]#, index = pd.to_datetime(x.iloc[2:, c])
+                # )
+                # tmp.index = pd.to_datetime(x.iloc[2:, c])
+                # tmp = tmp.reindex(pd.to_datetime(x.iloc[2:, c]), axis=0)
+                # tmp.isna()
+                # tmp.index.isna()
+                tmp = tmp.asfreq('600S')
+                tmp.columns.name = None
+                if res is None:
+                    res = tmp
+                else:
+                    res = pd.merge(res, tmp, how='outer', left_index=True, right_index=True)
+                del tmp
+        return res
+    res = clean(x)
+
+    res2 = clean(x2)
+    assert res2.shape[0] != 0, f'{fp} is empty'
+    d_max = res.index.max()
+
+    # test = pd.merge(res, res2, how='inner', left_index=True, right_index=True)
+    # dd = pd.DataFrame.from_dict(
+    #     {c: (np.abs(res.loc[test.index, c] - res2.loc[test.index, c])).mean() for c in res.columns},
+    #     orient='index'
+    # )
+    # dd.plot.barh()
+    # plt.show()
+    # d_max = res.merge(res2, how='inner', left_index=True, right_index=True).index.max()
+    # res.merge(res2.loc[res2.index > d_max], how='outer', left_index=True, right_index=True, indicator='False')
+
+    res = pd.concat([res, res2.loc[res2.index > d_max]])
+    res.to_csv(os.path.join(fp_clean, fx_name + '.csv'))
+    
